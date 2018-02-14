d7b9e547ea60a86dd6b011e70b8133fede7d0546
idabagusangga <angga.idabagus@gmail.com>
2018-02-14 11:32:34 +0700

    new server

---
+++ controllers/walmart.js
@@ -0,0 +1,30 @@
+const axios = require('axios')
+
+class WalmartController {
+  static getProducts (req, res) {
+    axios.get('http://api.walmartlabs.com/v1/search?query=iphone&format=json&apiKey=hsmapnzjjpgpwyg86c6423dk&numItems=4')
+    .then(response => {
+      let result = []
+      response.data.items.forEach(item => {
+        let prodObj = {
+          name : item.name,
+          price: item.salePrice,
+          description: item.shortDescription,
+          details: item.longDescription,
+          image: item.mediumImage
+        }
+        result.push(prodObj)
+      })
+      res.status(200).json({
+        data: result
+      })
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        err: err
+      })
+    })
+  }
+}
+module.exports = WalmartController;
